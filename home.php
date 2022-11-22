dbc1f79d0e3195b9f598479bd5d1a1499e9afcce
G-Faith <rgusti530@gmail.com>
2022-11-22 10:03:58 +0700

    edit edit dikit

--- home.php
+++ home.php
@@ -5,16 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
     <title>Gustoro Coffee Shop</title>
 </head>
 
-<body class="home-bg">
-    <header class="bg-dark py-5">
-        <nav class="nav navbar-expand-lg fixed-top p-5 navbar-dark">
+<body>
+    <header class=" py-5">
+        <nav class="nav navbar-expand-lg fixed-top p-4 navbar-light d-flex align-items-center
+            justify-content-center text-secondary">
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsMiddle" aria-controls="#navbarsMiddle" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsMiddle"
+                    aria-controls="#navbarsMiddle" aria-expanded="false" aria-label="Toggle navigation"><span
+                        class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse justify-content-md-center" id="navbarsMiddle">
                     <ul class="navbar-nav mb-2 mb-lg-0">
                         <li class="nav-item">
@@ -33,7 +37,7 @@
                 </div>
             </div>
         </nav>
-        <div class="container px-2">
+        <div class="container py-5 px-2">
             <div class="row gx-3 d-flex align-items-center justify-content-center">
                 <div class="col-6 col-lg-5 col-xl-5 col-xxl-4 ">
                     <div class="my-5 text-warning text-center text-xl-start">
@@ -44,7 +48,8 @@
                 </div>
                 <div class="col-6 col-xl-4 col-xxl-5 text-center">
                     <div>
-                        <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_wyopauto.json" background="transparent" speed="1" loop autoplay></lottie-player>
+                        <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_wyopauto.json"
+                            background="transparent" speed="1" loop autoplay></lottie-player>
                     </div>
                 </div>
             </div>
@@ -52,7 +57,9 @@
     </header>
 
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
